a47f07091dff862ee87cc1fcad25285671536e2d
Ray <raymundor22@gmail.com>
2019-09-28 07:52:23 -0500

    Login y registro

--- ProyectoBase/src/app/auth/registrar/registrar.page.ts
+++ ProyectoBase/src/app/auth/registrar/registrar.page.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarPage implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group(
+    {
+      nombre:['',Validators.required],
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.minLength(6)]],
+      confirmPassword:['',Validators.required],
+    },{
+      validators:this.MustMatch('password','confirmPassword')
+    }
+    );
+  }
+
+  registrar(form){
+    let inputs= form.form.value;
+    if(inputs.password != inputs.confirmPassword){
+      console.log('El password y la confirmcion')
+    }
+  }
+
+  MustMatch(controlName:string, matchingControlName:string){
+    return true;
   }
 
 }
